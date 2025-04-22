ab519579b454319e6b7a922565b5c2fd606fe452
Adarsh Nambiath <adarshnambiath@gmail.com>
2025-04-22 09:44:41 +0530

    trying to connect frontend with render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:8080');
+const socket = io('https://hangman-backend.onrender.com');
 
 function App() {
   const [room, setRoom] = useState('');
